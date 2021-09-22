2097c146ed75db66cb08428fd817c9be7ce3a14a
Nicola <nicola.taddei.94@gmail.com>
2021-09-22 16:44:48 +0200

    chore(single spa): added

--- packages/dashboard/src/app/app.module.ts
+++ packages/dashboard/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EmptyRouteComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
