3de69f27b665c749e79500b6d391488b4d8063ad
Nicola <nicola.taddei.94@gmail.com>
2021-09-27 12:22:04 +0200

    feat(shared:models): added user interface

--- packages/shared/projects/models/src/public-api.ts
+++ packages/shared/projects/models/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of models
- */
-
-export * from './lib/models.service';
-export * from './lib/models.component';
-export * from './lib/models.module';
+export * from './lib/users';
