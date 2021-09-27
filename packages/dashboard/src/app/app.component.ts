0f0a9c3fb75f0813e85eceef287f164ecbd264ba
Nicola <nicola.taddei.94@gmail.com>
2021-09-27 13:20:38 +0200

    chore(dashboard:paths): added shared models as tsconfig paths

--- packages/dashboard/src/app/app.component.ts
+++ packages/dashboard/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Users } from '@models';
 
 @Component({
   selector: 'dashboard-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'dashboard';
+  user: Users;
+
+  constructor() {
+    this.user = { username: 'Franco' };
+  }
 }
